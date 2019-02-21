77ce7c9bbf03710699a40f4333e7e66efece1b22
github username <github email address>
2019-02-21 09:27:16 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    find_article
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    find_article
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    find_article
+    @article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    find_article
+    @article.delete
+  end
+
+  # helper method.
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
